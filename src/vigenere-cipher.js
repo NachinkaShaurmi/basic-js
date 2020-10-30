7f5a66537ee60c1c8407572069d2b5ea31ba8e22
Yuriy <agent250691@yandex.ru>
2020-10-30 10:20:55 +0300

    feat: exception vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text, key) {
+    if (arguments.length < 2 || text == undefined || key == undefined) throw new Error;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(text, key) {
+    if (arguments.length < 2 || text == undefined || key == undefined) throw new Error;
   }
 }
 
